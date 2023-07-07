e0c915fe4be6caad704fb5a187c11d81a5841028
duindain <duindain@gmail.com>
2023-07-07 15:24:37 +1000

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NavigationEllipsis;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+#if WINDOWS
+using Windows.UI.ViewManagement.Core;
+#endif
+
+namespace NavigationEllipsis;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+#if WINDOWS
+            //From https://github.com/dotnet/maui/issues/12063 work around for WinUI toolbar issue
+            Microsoft.Maui.Handlers.ToolbarHandler.Mapper.Add("FixPlacement", (handler, view) =>
+            {
+                handler.PlatformView.DefaultLabelPosition = Microsoft.UI.Xaml.Controls.CommandBarDefaultLabelPosition.Right;
+            });
+#endif
+        return builder.Build();
+	}
+}
